543d4c23b49cec9233b2e389b792488e868b883d
bluesky7660 <chlee7660@gmail.com>
2024-11-23 22:09:33 +0900

    1123 3

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,26 @@
+const { overrideDevServer } = require('customize-cra');
 const path = require('path');
-
-module.exports = function override(config) {
-  // Webpack이 src 폴더를 기준으로 경로를 인식하도록 설정
-  config.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules'];
-
-  // 경로 별칭을 Webpack에 명시적으로 추가
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    '@': path.resolve(__dirname, 'src'),
-    '@etc': path.resolve(__dirname, 'src/etc'),
-    '@firebaseApi': path.resolve(__dirname, 'src/firebase'),
-    '@context': path.resolve(__dirname, 'src/context'),
-    '@hooks': path.resolve(__dirname, 'src/core/hooks'),
-    '@router': path.resolve(__dirname, 'src/feature-module/router')
-  };
-
+const devServerConfig = () => (config) => {
+  config.port = 8083; // 원하는 포트 번호
   return config;
+};
+module.exports = {
+  webpack: function override(config) {
+    // Webpack이 src 폴더를 기준으로 경로를 인식하도록 설정
+    config.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules'];
+    process.env.PORT = 3001;
+    // 경로 별칭을 Webpack에 명시적으로 추가
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@etc': path.resolve(__dirname, 'src/etc'),
+      '@firebaseApi': path.resolve(__dirname, 'src/firebase'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@hooks': path.resolve(__dirname, 'src/core/hooks'),
+      '@router': path.resolve(__dirname, 'src/feature-module/router')
+    };
+
+    return config;
+  },
+  devServer: overrideDevServer(devServerConfig()),
 };
